13dcec1aad3b11c54927dd8c81fa360178fb0db4
shun-harutaro <shun_harutaro@protonmail.com>
2023-08-13 17:41:26 +0900

    Express経由でReactを表示できた

--- src/server/index.js
+++ src/server/index.js
@@ -1,19 +1,23 @@
 const express = require("express");
-const functions = require("firebase-functions");
+//const functions = require("firebase-functions");
 const axios = require('axios');
 const app = express();
 const path = require("path");
-//const port = 3001;
+const port = 3001;
 require('dotenv').config();
 
 const REDIRECT_URI = process.env.REDIRECT_URI;
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
+app.use(express.static(path.resolve('./', 'dist')));
+
+/*
 app.get("/", (req, res) => {
     //res.send('<h1>LINEログインテストアプリ</h1><a href="/login">LINEでログイン</a>');
     res.sendFile(path.resolve('./', 'build', 'index.html'))
 })
+*/
 
 app.get("/login", (req, res) => {
     let baseUrl = "https://access.line.me/oauth2/v2.1/authorize";
@@ -66,10 +70,15 @@ app.get("/callback", (req, res) => {
         }
     })();
 })
-/*
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve('./', 'dist', 'index.html'))
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 })
-*/
 
+/*
 exports.app = functions.https.onRequest(app);
+*/
